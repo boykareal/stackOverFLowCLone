90f6d66b20d390961c2d6782070138d72d21b2d2
boykareal <vaibhavx3sharma@gmail.com>
2026-07-14 02:05:51 +0530

    add question collection

---
+++ src/models/server/question.collection.ts
@@ -0,0 +1,43 @@
+import {Permission, DatabasesIndexType, OrderBy} from "node-appwrite"
+import {db, questionCollection} from "../name"
+import {databases} from "./config"
+
+
+export default async function createQuestionCollection(){
+    await databases.createCollection(db, questionCollection, questionCollection, [
+        Permission.read("any"),
+        Permission.read("users"),
+        Permission.create("users"),
+        Permission.update("users"),
+        Permission.delete("users")
+    ])
+    console.log("Question collection is created")
+
+    await Promise.all([
+        databases.createStringAttribute(db, questionCollection, "title", 100, true),
+        databases.createStringAttribute(db, questionCollection, "content", 10000, true),
+        databases.createStringAttribute(db, questionCollection, "authorId", 50, true),
+        databases.createStringAttribute(db, questionCollection, "tags", 50, true, undefined, true),
+        databases.createStringAttribute(db, questionCollection, "attachmentId", 50, false),
+    ])
+    console.log("Questions Attributes created")
+
+    await Promise.all([
+        databases.createIndex({
+            databaseId: db,
+            collectionId: questionCollection,
+            key: "title",
+            type: DatabasesIndexType.Fulltext,
+            attributes: ["title"],
+            orders: [OrderBy.Asc],
+        }),
+        databases.createIndex({
+            databaseId: db,
+            collectionId: questionCollection,
+            key: "content",
+            type: DatabasesIndexType.Fulltext,
+            attributes: ["content"],
+            orders: [OrderBy.Asc]
+        })
+    ])
+}
